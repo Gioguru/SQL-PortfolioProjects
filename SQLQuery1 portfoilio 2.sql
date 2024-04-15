196c3865db64f58137d9bd66f6dfe4ba04ff9776
GC <135825161+Gioguru@users.noreply.github.com>
2024-04-16 01:56:08 +0400

    Add files via upload

---
+++ SQLQuery1 portfoilio 2.sql
@@ -0,0 +1,17 @@
+
+with hotels as (
+select * from dbo.['2018$']
+union
+select * from dbo.['2019$']
+union
+select * from dbo.['2020$'])
+
+select * from hotels
+left join dbo.market_segment$ 
+on hotels.market_segment = market_segment$.market_segment
+left join
+dbo.meal_cost$
+on dbo.meal_cost$.meal = hotels.meal
+
+
+
